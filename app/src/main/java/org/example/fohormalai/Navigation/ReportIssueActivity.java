2b12f153d9a48dbc3bd43f7dd347d2fe6115e754
Binay Babu Prajapati <prajapatibnay@gmail.com>
2018-09-20 16:10:35 +0545

    Second commit

--- app/src/main/java/org/example/fohormalai/Navigation/ReportIssueActivity.java
+++ app/src/main/java/org/example/fohormalai/Navigation/ReportIssueActivity.java
@@ -1,5 +1,6 @@
 package org.example.fohormalai.Navigation;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBar;
@@ -7,7 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.example.fohormalai.Login;
 import org.example.fohormalai.R;
@@ -17,11 +22,21 @@ public class ReportIssueActivity extends AppCompatActivity {
 
     private TextView et_attach, et_capture;
     private static int RESULT_LOAD_IMAGE = 1;
+    private final static int Pick_Image = 1;
+    private final static int Capture_Image = 1;
+    private Uri mImageUri;
+    private ImageView attachimage;
+    private ImageView captureimage;
+    Button btn_report;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_issue);
+
+        attachimage = findViewById(R.id.imgView1);
+        captureimage = findViewById(R.id.imgView2);
 
         // get action bar
         ActionBar actionBar = getSupportActionBar();
@@ -32,6 +47,7 @@ public class ReportIssueActivity extends AppCompatActivity {
 
         et_attach = (TextView)findViewById(R.id.insert);
         et_capture = (TextView)findViewById(R.id.capture);
+        btn_report = (Button) findViewById(R.id.btn_report);
 
         et_attach.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +66,32 @@ public class ReportIssueActivity extends AppCompatActivity {
                 startActivity(capt);
             }
         });
+
+        btn_report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:clustered.concepts018@gmail.com"));
+                startActivity(emailIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Pick_Image && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(attachimage);
+            //* or mImageView.setImageUri(mImageUri); if Picasso is not used*//
+
+        }
+
+        else if (requestCode == Capture_Image && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+            Picasso.get().load(mImageUri).into(captureimage);
+        }
     }
 
     @Override
